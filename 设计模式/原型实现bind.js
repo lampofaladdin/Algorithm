89add192030ee53fe0d3d8a153f8f5f571b1f1c6
aladdin <1295106869@qq.com>
2020-07-10 15:29:00 +0800

    做题

--- 设计模式/原型实现bind.js
+++ 设计模式/原型实现bind.js
@@ -15,17 +15,27 @@
 // // console.log([].shift.call([1, 2, 3]));
 // console.log([].slice.call([1, 2, 3]));
 
-var Type = {};
-for (var i = 0, type; (type = ["String", "Array", "Number"][i++]); ) {
-  (function (type) {
-      console.log(type)
-    Type["is" + type] = function (obj) {
-      return Object.prototype.toString.call(obj) === "[object " + type + "]";
-    };
-  })(type);
-}
-console.log(Type)
-// Type.isArray([]); // 输出:true
-// console.log(Type.isArray([]))
-// Type.isString("str");
-// // 输出:true
+// var Type = {};
+// for (var i = 0, type; (type = ["String", "Array", "Number"][i++]); ) {
+//   (function (type) {
+//       console.log(type)
+//     Type["is" + type] = function (obj) {
+//       return Object.prototype.toString.call(obj) === "[object " + type + "]";
+//     };
+//   })(type);
+// }
+// console.log(Type)
+// // Type.isArray([]); // 输出:true
+// // console.log(Type.isArray([]))
+// // Type.isString("str");
+// // // 输出:true
+
+// Number.prototype.add = function (num) {
+//   return this + num;
+// };
+
+// Number.prototype.minus = function (num) {
+//   return this - num;
+// };
+
+// console.log((5).add(3).minus(2));
